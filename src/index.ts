11f4fb0691185621c94daff69e1b7ea2de4536a8
Andy Brunner <andy@cloudxs.ch>
2022-02-16 10:38:42 +0100

    first class with IAM role for Wireless gateway

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
+import { Role, ManagedPolicy, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+
+/**
+ * Creates a role called 'IoTWirelessGatewayCertManagerRole' that is needed by the IoT Wireless gateway
+ * https://docs.aws.amazon.com/iot/latest/developerguide/connect-iot-lorawan-rfregion-permissions.html#connect-iot-lorawan-onboard-permissions
+ */
+export class IotWirelessGatewayRole extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    new Role(this, 'IoTWirelessGatewayCertManagerRole', {
+      roleName: 'IoTWirelessGatewayCertManagerRole',
+      assumedBy: new ServicePrincipal('iotwireless.amazonaws.com'),
+      managedPolicies: [
+        ManagedPolicy.fromAwsManagedPolicyName('AWSIoTWirelessGatewayCertManager'),
+      ],
+    });
+
   }
 }
